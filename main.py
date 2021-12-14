97aa6e0dbd1816b30e7a3c6afb40dc9d3ceaed78
Lazic99H <80473608+markokovljen@users.noreply.github.com>
2021-12-14 23:48:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ from database.config import db,ma
 app = Flask(__name__)
 app.register_blueprint(bp_sign)
 
-
-app.config["SQLALCHEMY_DATABASE_URL"] = "mysql+mysqlconnector://root:123@0.0.0.0/dresbase"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:123@0.0.0.0/dresdata'
 db.init_app(app)
 ma.init_app(app)
 
 @app.route('/')
 def main():
+
     return "<h1>Main funk</h1>"
 
 if __name__ == "__main__":
