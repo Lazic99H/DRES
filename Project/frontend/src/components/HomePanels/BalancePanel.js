d4f92dbb67ef9cd01bb57179f746d896e649aadf
Lazic99H <79918874+bozidarsredic@users.noreply.github.com>
2022-01-06 13:29:39 +0100

    Home

--- Project/frontend/src/components/HomePanels/BalancePanel.js
+++ Project/frontend/src/components/HomePanels/BalancePanel.js
@@ -13,19 +13,50 @@ import React from "react";
 function BalancePanel () {
     return (
     <Chart>
-        <ChartTitle text={"Asset Allocation"}></ChartTitle>
-        <ChartSeries>
-            <ChartSeriesItem type="donut" data={Number(sessionStorage.getItem("balance"))}>
-            <ChartSeriesLabels
-                content={e => `${e.value}%`}
-                background="none"
-                color="#fff" />
-            </ChartSeriesItem>
-        </ChartSeries>
-        <ChartLegend position={"bottom"} visible={true} />
-        <ChartTooltip render={(e: any) => (
-            <div>{e.point ? e.point.category : ""}</div>
-        )} />
+      <ChartTitle text={"Asset Allocation"}></ChartTitle>
+      <ChartSeries>
+        <ChartSeriesItem type="donut"
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }>
+        <ChartSeriesLabels
+          content={e => `${e.value}%`}
+          background="none"
+          color="#fff" />
+        </ChartSeriesItem>
+      </ChartSeries>
+      <ChartLegend position={"bottom"} visible={true} />
+      <ChartTooltip render={(e: any) => (
+        <div>{e.point ? e.point.category : ""}</div>
+      )} />
     </Chart>
     );
 }
